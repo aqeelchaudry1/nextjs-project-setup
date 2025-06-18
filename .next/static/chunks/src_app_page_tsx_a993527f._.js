64af5db138c69d93e49004086d686bed7a51bcf6
aqeelchaudry1 <aqeelchaudry888@gmail.com>
2025-06-18 23:30:26 +0400

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1750275026046

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_4e3cad3e._.js",
+  "static/chunks/src_bd14d733._.js"
+],
+    source: "dynamic"
+});
