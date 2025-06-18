126ae31d7cbba94fe1b05b0d6838612f6da3ca6f
aqeelchaudry1 <aqeelchaudry888@gmail.com>
2025-06-19 02:35:13 +0400

    Add next.config.ts 1750286112450

---
+++ next.config.ts
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/photos/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  allowedDevOrigins: ['lxrmfq-8000.csb.app'],
+}
+
+export default nextConfig;
